2f56179dee264412ce3c0f673716cb4828f2f122
André Jr <andrejr.mello@hotmail.com>
2024-01-31 16:07:03 -0300

    Entidade User e seu Resource

---
+++ src/main/java/com/amdrejr/springlojabackend/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.amdrejr.springlojabackend.resources;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.amdrejr.springlojabackend.entities.User;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "João", "123123", "joao@gmail.com", "joao123");
+        return ResponseEntity.ok().body(u);
+    }
+}
