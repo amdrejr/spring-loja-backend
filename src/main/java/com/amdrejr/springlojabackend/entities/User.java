1fe324faccb204f1fa9e272dddf424ebfb13249f
André Jr <andrejr.mello@hotmail.com>
2024-02-01 17:20:21 -0300

    Order, OrderController, OrderService, OrderRepository, enum OrderStatus

--- src/main/java/com/amdrejr/springlojabackend/entities/User.java
+++ src/main/java/com/amdrejr/springlojabackend/entities/User.java
@@ -1,11 +1,16 @@
 package com.amdrejr.springlojabackend.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,6 +23,9 @@ public class User implements Serializable {
     private String phone;
     private String email;
     private String password;
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {}
 
@@ -67,6 +75,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
---
+++ src/main/java/com/amdrejr/springlojabackend/services/OrderService.java
@@ -0,0 +1,24 @@
+package com.amdrejr.springlojabackend.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.amdrejr.springlojabackend.entities.Order;
+import com.amdrejr.springlojabackend.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    public Order findById(Long id) {
+        return orderRepository.findById(id).orElse(null);
+    }
+}
+
